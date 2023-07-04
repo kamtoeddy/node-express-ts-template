cffe9469cae54c092ab4b4a542e9fcf0498d24a1
kamtoeddy <kamtoeddytagne@gmail.com>
2023-07-04 08:39:40 +0100

    feat: add services folder & update types

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+import type { AuthInfo } from './types'
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  namespace Express {
+    export interface Request {
+      auth: AuthInfo
+    }
+  }
+}
+
+export {}
